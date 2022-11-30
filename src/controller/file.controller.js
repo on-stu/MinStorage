9bf0ad9eb45b3b052472177c41deeda6b82a218f
김민수 <minsu0523@naver.com>
2022-12-01 01:58:48 +0900

    safari issue

--- src/controller/file.controller.js
+++ src/controller/file.controller.js
@@ -101,27 +101,103 @@ const downloadFile = (filePath, res) => {
   res.download(filePath);
 };
 
-const streamFile = (req, res, videoPath) => {
-  const range = req.headers.range;
+const streamFile = (req, res, filePath) => {
+  // const range = req.headers.range;
 
-  if (range == undefined) {
-    res.status(400).send("Requires Range header");
-    return;
+  // if (range == undefined) {
+  //   res.status(400).send("Requires Range header");
+  //   return;
+  // }
+  // const videoSize = fs.statSync(videoPath).size;
+  // const CHUNK_SIZE = 10 ** 6;
+  // const start = Number(range.replace(/\D/g, ""));
+  // const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+  // const contentLength = end - start + 1;
+  // const headers = {
+  //   "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+  //   "Accept-Ranges": "bytes",
+  //   "Content-Length": contentLength,
+  //   "Content-Type": "video/mp4",
+  // };
+  // res.writeHead(206, headers);
+  // const videoStream = fs.createReadStream(videoPath, { start, end });
+  // videoStream.pipe(res);
+
+  const options = {};
+
+  let start;
+  let end;
+
+  const range = req.headers.range;
+  if (range) {
+    const bytesPrefix = "bytes=";
+    if (range.startsWith(bytesPrefix)) {
+      const bytesRange = range.substring(bytesPrefix.length);
+      const parts = bytesRange.split("-");
+      if (parts.length === 2) {
+        const rangeStart = parts[0] && parts[0].trim();
+        if (rangeStart && rangeStart.length > 0) {
+          options.start = start = parseInt(rangeStart);
+        }
+        const rangeEnd = parts[1] && parts[1].trim();
+        if (rangeEnd && rangeEnd.length > 0) {
+          options.end = end = parseInt(rangeEnd);
+        }
+      }
+    }
   }
-  const videoSize = fs.statSync(videoPath).size;
-  const CHUNK_SIZE = 10 ** 6;
-  const start = Number(range.replace(/\D/g, ""));
-  const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
-  const contentLength = end - start + 1;
-  const headers = {
-    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-    "Accept-Ranges": "bytes",
-    "Content-Length": contentLength,
-    "Content-Type": "video/mp4",
-  };
-  res.writeHead(206, headers);
-  const videoStream = fs.createReadStream(videoPath, { start, end });
-  videoStream.pipe(res);
+
+  res.setHeader("content-type", "video/mp4");
+
+  fs.stat(filePath, (err, stat) => {
+    if (err) {
+      console.error(`File stat error for ${filePath}.`);
+      console.error(err);
+      res.sendStatus(500);
+      return;
+    }
+
+    let contentLength = stat.size;
+
+    if (req.method === "HEAD") {
+      res.statusCode = 200;
+      res.setHeader("accept-ranges", "bytes");
+      res.setHeader("content-length", contentLength);
+      res.end();
+    } else {
+      let retrievedLength;
+      if (start !== undefined && end !== undefined) {
+        retrievedLength = end + 1 - start;
+      } else if (start !== undefined) {
+        retrievedLength = contentLength - start;
+      } else if (end !== undefined) {
+        retrievedLength = end + 1;
+      } else {
+        retrievedLength = contentLength;
+      }
+
+      res.statusCode = start !== undefined || end !== undefined ? 206 : 200;
+
+      res.setHeader("content-length", retrievedLength);
+
+      if (range !== undefined) {
+        res.setHeader(
+          "content-range",
+          `bytes ${start || 0}-${end || contentLength - 1}/${contentLength}`
+        );
+        res.setHeader("accept-ranges", "bytes");
+      }
+
+      const fileStream = fs.createReadStream(filePath, options);
+      fileStream.on("error", (error) => {
+        console.log(`Error reading file ${filePath}.`);
+        console.log(error);
+        res.sendStatus(500);
+      });
+
+      fileStream.pipe(res);
+    }
+  });
 };
 
 const remove = (req, res) => {
